5f8a10cee96e839ab64304b0dd35bc2b3b407700
Oladipo Adesiyan <adesiyanoladipo1@gmail.com>
2021-09-02 07:57:06 +0100

    Delete home directory

--- home/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render, redirect
-from django.http import HttpResponse
-
-# Create your views here.
-def home(request):
-	template = "home.html"
-	return render (request, template)
